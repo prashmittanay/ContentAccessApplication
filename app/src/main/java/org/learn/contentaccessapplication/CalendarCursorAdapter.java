d002ad5929dc7f307591872e06c6f06a3edd7c53
prashmittanay <prashmittanay@gmail.com>
2020-09-26 18:46:12 +0530

    bind() method implemented for Calendar cursor adapter

--- app/src/main/java/org/learn/contentaccessapplication/CalendarCursorAdapter.java
+++ app/src/main/java/org/learn/contentaccessapplication/CalendarCursorAdapter.java
@@ -2,9 +2,13 @@ package org.learn.contentaccessapplication;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 public class CalendarCursorAdapter extends CursorAdapter {
     public CalendarCursorAdapter(Context context, Cursor c, int flags) {
@@ -13,11 +17,17 @@ public class CalendarCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        return null;
+        return LayoutInflater.from(context)
+                .inflate(R.layout.layout_calendar_events_list, viewGroup, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView calendarEventTextView = view.findViewById(R.id.text_calendar_event);
 
+        String eventName = cursor.getString(cursor.getColumnIndex(
+                CalendarContract.Instances.TITLE));
+
+        calendarEventTextView.setText(eventName);
     }
 }
