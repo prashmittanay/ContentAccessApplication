87f6dfd6ec808bc50be3c70649e7ec7d4ef1dca3
prashmittanay <prashmittanay@gmail.com>
2020-09-26 20:03:39 +0530

    A new activity dedicated to contacts added

--- app/src/main/java/org/learn/contentaccessapplication/MainActivity.java
+++ app/src/main/java/org/learn/contentaccessapplication/MainActivity.java
@@ -21,84 +21,12 @@ import android.provider.ContactsContract;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    private ListView mListView;
-    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mListView = (ListView) findViewById(R.id.list_main_contacts);
-
-        requestContactsPermission();
     }
-
-    @NonNull
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
-        Uri contentUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        CursorLoader cursorLoader = new CursorLoader(this, contentUri, null,
-                null, null, null);
-        return cursorLoader;
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
-        cursor.moveToFirst();
-        ContactsCursorAdapter contactsCursorAdapter = new ContactsCursorAdapter(this,
-                cursor, 0);
-        mListView.setAdapter(contactsCursorAdapter);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_READ_CONTACTS:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getContacts();
-                } else {
-                    Toast.makeText(this, "Contact Permission Disabled!", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            default:
-        }
-    }
-
-    public void requestContactsPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Read Contacts Permission");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setMessage("Please enable access to contacts");
-                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialogInterface) {
-                            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
-                        }
-                    });
-                    builder.show();
-                } else {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
-                }
-            } else {
-                getContacts();
-            }
-        } else {
-            getContacts();
-        }
-    }
-
-    public void getContacts() {
-        LoaderManager.getInstance(this).initLoader(1, null, this);
-    }
-
 }
