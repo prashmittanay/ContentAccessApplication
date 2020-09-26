a011dd65fcf97a64b2a6677d14c1ec9c7da99b6d
prashmittanay <prashmittanay@gmail.com>
2020-09-26 17:37:11 +0530

    added cursorloader to load contacts data

--- app/src/main/java/org/learn/contentaccessapplication/MainActivity.java
+++ app/src/main/java/org/learn/contentaccessapplication/MainActivity.java
@@ -1,14 +1,49 @@
 package org.learn.contentaccessapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.list_main_contacts);
+
+        LoaderManager.getInstance(this).initLoader(1, null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        Uri contentUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        CursorLoader cursorLoader = new CursorLoader(this, contentUri, null,
+                null, null, null);
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        ContactsCursorAdapter contactsCursorAdapter = new ContactsCursorAdapter(this,
+                cursor, 0);
+        mListView.setAdapter(contactsCursorAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
