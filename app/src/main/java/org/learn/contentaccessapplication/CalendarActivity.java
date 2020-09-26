d74a61409b42819700e3b202b8d1bd36c338785f
prashmittanay <prashmittanay@gmail.com>
2020-09-26 20:20:05 +0530

    dynamic permissions for Calendar activity added

--- app/src/main/java/org/learn/contentaccessapplication/CalendarActivity.java
+++ app/src/main/java/org/learn/contentaccessapplication/CalendarActivity.java
@@ -2,23 +2,31 @@ package org.learn.contentaccessapplication;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 
+import android.Manifest;
 import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.provider.ContactsContract;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     private ListView mListView;
+    public static final int PERMISSIONS_REQUEST_READ_CALENDAR = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,8 @@ public class CalendarActivity extends AppCompatActivity implements LoaderManager
         setContentView(R.layout.activity_calendar);
 
         mListView = (ListView) findViewById(R.id.list_calendar_events);
-        LoaderManager.getInstance(this).initLoader(1, null, this);
+
+        requestCalendarPermission();
     }
 
     @NonNull
@@ -63,5 +72,50 @@ public class CalendarActivity extends AppCompatActivity implements LoaderManager
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CALENDAR:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts();
+                } else {
+                    Toast.makeText(this, "Contact Permission Disabled!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            default:
+        }
+    }
+
+    public void requestCalendarPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CALENDAR)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Read Calendar Permission");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please enable access to Calendar");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialogInterface) {
+                            requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, PERMISSIONS_REQUEST_READ_CALENDAR);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, PERMISSIONS_REQUEST_READ_CALENDAR);
+                }
+            } else {
+                getContacts();
+            }
+        } else {
+            getContacts();
+        }
+    }
+
+    public void getContacts() {
+        LoaderManager.getInstance(this).initLoader(1, null, this);
     }
 }
