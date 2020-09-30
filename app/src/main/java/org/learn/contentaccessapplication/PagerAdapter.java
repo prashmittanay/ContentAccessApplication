e376621acff633e18fbe06dcb4d012da588cf5c7
prashmittanay <prashmittanay@gmail.com>
2020-09-30 19:40:08 +0530

    view pager added and activity main modified

--- app/src/main/java/org/learn/contentaccessapplication/PagerAdapter.java
+++ app/src/main/java/org/learn/contentaccessapplication/PagerAdapter.java
@@ -1,4 +1,35 @@
 package org.learn.contentaccessapplication;
 
-public class PagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fragmentManager, int NumOfTabs) {
+        super(fragmentManager);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                CalendarFragment calendarFragment = new CalendarFragment();
+                return calendarFragment;
+            case 1:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
 }
