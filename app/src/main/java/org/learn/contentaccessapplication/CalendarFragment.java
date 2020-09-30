a4b6584372d34d9e7a2fbdd5ed549ad618c102e9
prashmittanay <prashmittanay@gmail.com>
2020-09-30 19:19:08 +0530

    Calendar Fragment created

---
+++ app/src/main/java/org/learn/contentaccessapplication/CalendarFragment.java
@@ -0,0 +1,128 @@
+package org.learn.contentaccessapplication;
+
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+
+import java.util.Calendar;
+
+public class CalendarFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private ListView mListView;
+    private TextView mTextView;
+    public static final int PERMISSIONS_REQUEST_READ_CALENDAR = 1;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mTextView = (TextView) container.findViewById(R.id.emptyElementCalendar);
+        mListView = (ListView) container.findViewById(R.id.list_calendar_events);
+        mListView.setEmptyView(mTextView);
+
+        requestCalendarPermission();
+
+        return inflater.inflate(R.layout.activity_calendar, container, false);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DATE), 0, 0);
+        long startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DATE), 23, 59);
+        long endMillis = endTime.getTimeInMillis();
+
+
+        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+        ContentUris.appendId(builder, startMillis);
+        ContentUris.appendId(builder, endMillis);
+
+        String selection = CalendarContract.Events.DTSTART + " > ? AND " + CalendarContract.Events.DTEND + " < ?";
+        String[] selectionArgs = {String.valueOf(startMillis), String.valueOf(endMillis)};
+
+        CursorLoader cursorLoader = new CursorLoader(getContext(), CalendarContract.Events.CONTENT_URI, null,
+                selection, selectionArgs, null);
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        CalendarCursorAdapter calendarCursorAdapter = new CalendarCursorAdapter(getContext(),
+                cursor, 0);
+        mListView.setAdapter(calendarCursorAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CALENDAR:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts();
+                } else {
+                    Toast.makeText(getContext(), "Contact Permission Disabled!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            default:
+        }
+    }
+
+    public void requestCalendarPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_CALENDAR)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Read Calendar Permission");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please enable access to Calendar");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialogInterface) {
+                            requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, PERMISSIONS_REQUEST_READ_CALENDAR);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CALENDAR}, PERMISSIONS_REQUEST_READ_CALENDAR);
+                }
+            } else {
+                getContacts();
+            }
+        } else {
+            getContacts();
+        }
+    }
+
+    public void getContacts() {
+        LoaderManager.getInstance(this).initLoader(1, null, this);
+    }
+}
